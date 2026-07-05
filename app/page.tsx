edc857648f29668af2375bd0e1107c01bae14568
Arman Yadav <armanofficial275@gmail.com>
2026-07-06 01:54:22 +0530

    Create reusable Navbar component and add routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,48 @@
 import Link from "next/link";
-import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="min-h-screen">
 
-     <Navbar />
+ 
 
       <section className="flex flex-col items-center justify-center text-center mt-32">
 
-        <h2 className="text-6xl font-bold">
-          Share your stories with the world 
-        </h2>
+        <h1 className="text-6xl font-bold">
+          Every story deserves a home 
+        </h1>
 
-        <p className="mt-6 text-xl max-w-2xl text-gray-600">
-          Discover stories, ideas and knowledge from
-          writers around the world.
+       <h2 className="mt-8">
+        This week's Reflection 
+       </h2>
+
+       <p className="mt-4 text-gray-600"> "There is no greater agony than bearing an untold story inside you."
         </p>
 
-        <button className="mt-10 px-8 py-4 bg-blue text-white rounded-xl hover:scale-105 transition">
-          Start writing
-        </button>
+        <p className="mt-4 text-gray-600">
+        - Maya Angelou
+        </p>
 
+<p className="mt-4">Turn the page and let your story unfold.</p>
+
+<p className="mt-4">↓</p>
       </section>
 
+      <section className="flex flex-col items-center justify-center text-center mt-32">
+        <h1 className="text-6xl font-bold">
+        Where Every Story Finds a Home
+        </h1>
+        <p>
+
+<p className="mt-4"></p>──────── ✦ ────────</p>
+
+        <p className="mt-4 text-gray-600">Some stories are spoken aloud. Others are quietly carried in hearts, journals, letters, and unfinished pages. Every story, whether joyful or heartbreaking, becomes a part of the person who lives it.
+        </p>
+<p className="mt-4 text-gray-600">Inkspire exists as a bridge for those stories—a place where words can find a home and people can find one another through them. Not to judge, not to compete, but simply to remind us that every voice has value.</p>
+
+<p className="mt-4 text-gray-600">Whether you arrive with a poem, a memory, a lesson, or a chapter of your life that has never been shared before, there will always be a place for it here. Welcome home.</p>
+   </section>
+
+
     </main>
-  );
+  );    
 }
